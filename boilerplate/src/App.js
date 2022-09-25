c5e719e55b4219df1748a8c6e5510e8eab8ba38c
root <root@5521694.lesfleursdelanuit.tech>
2022-09-25 15:48:12 -0500

    generator works

--- boilerplate/src/App.js
+++ boilerplate/src/App.js
@@ -1,6 +1,9 @@
 import logo from './logo.svg';
+import AppModel from "./models/App/AppModel.js";
 import './App.css';
 
+
+const app_model = new AppModel(); 
 function App() {
   return (
     <div className="App">
